9136b780938893d2cc7a280cf56ad940871c7c0f
Ali Rojas <asarova@gmail.com>
2020-11-20 02:38:23 -0500

    Primera acometida

---
+++ carpeta/inicio.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <variant>
+
+using namespace std;
+#define SL cout<<endl
+
+//typedef variant<int,float> DATO;
+
+int main(void)
+{
+	cout << "\n\t\t >> ASAROVA << \n\n";
+
+
+    /*//
+	char s[] = "Hola todos";
+	s[1] = 'O';
+	for(auto i: s) cout << i;
+    //*/
+
+    /*//
+    DATO d = 7.8f;
+    cout << get<float>(d);
+    //*/
+
+	return 86;
+}
